8b932445f629bfdbf39ede96c32d3856cf78ad98
sampada-12 <sampadajadhav012@gmail.com>
2026-05-16 16:36:54 +0530

    done with show code and color and background color

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // value -> optional value for commands like link, color, font size
 
 const formatDoc = (cmd, value = false) => {
-    
+
   // Check if command needs a value
   if (value) {
-   
+
     document.execCommand(cmd, false, value);
   } else {
     
@@ -26,19 +26,48 @@ const handleAddLink = () => {
 
 let content = document.getElementById("content");
 content.addEventListener("mouseenter", () => {
-	let anchors = content.querySelectorAll("a");
+  let anchors = content.querySelectorAll("a");
 
-	anchors.forEach((anchor) => {
-		anchor.addEventListener("mouseenter", (e) => {
-			anchor.setAttribute("target", "_blank");
-			content.setAttribute("contentEditable", "false");
-		});
+  anchors.forEach((anchor) => {
+    anchor.addEventListener("mouseenter", (e) => {
+      anchor.setAttribute("target", "_blank");
+      content.setAttribute("contentEditable", "false");
+    });
 
-		anchor.addEventListener("mouseleave", (e) => {
-			content.setAttribute("contentEditable", "true");
-		});
-	});
+    anchor.addEventListener("mouseleave", (e) => {
+      content.setAttribute("contentEditable", "true");
+    });
+  });
 });
 
 
+// Track code mode state
+let active = false;
+// Select "Show Code" button
+let showCode = document.getElementById("show-code");
 
+// Detect button click
+showCode.addEventListener("click", () => {
+  
+  // Toggle active state (true/false)
+  active = !active;
+
+   // Update data-active attribute
+  showCode.dataset.active = active;
+
+   // If code mode is ON
+  if (active) {
+   // Show HTML code as plain text
+    content.textContent = content.innerHTML;
+
+    // Disable editor editing
+    content.setAttribute("contenteditable", "false"); } else {
+
+    // Convert plain text back into HTML
+    content.innerHTML = content.textContent;
+
+    // Enable editing again
+    content.setAttribute("contenteditable", "true");
+  }
+  
+});
